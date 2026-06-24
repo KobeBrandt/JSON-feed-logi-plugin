f4780da74be0cf610c0d586ba51a698b19581e66
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2026-06-24 13:21:34 +0200

    Add initial implementation of JSON Feed Plugin with core structure and basic functionality

---
+++ src/Actions/ListOfArticles.cs
@@ -0,0 +1,82 @@
+﻿namespace Loupedeck.JsonFeedPlugin.Actions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
+    using Loupedeck.JsonFeedPlugin.Helpers;
+
+    public class ListOfArticles : PluginDynamicFolder
+    {
+        private List<Article> articles;
+        private ArticleSettings settings;
+        private static ListOfArticles _instance;
+        
+        public static ListOfArticles Instance => _instance;
+        
+        public ListOfArticles()
+        {
+            this.DisplayName = "JSON Feed";
+            this.GroupName = "JSON Feed";
+            _instance = this;
+        }
+        public override PluginDynamicFolderNavigation GetNavigationArea(DeviceType _)
+        {
+            return PluginDynamicFolderNavigation.EncoderArea;
+        }
+         public override Boolean Load()
+         {
+             this.ReloadArticles();
+             return true;
+         }
+         
+           public void ReloadArticles()
+           {
+               try
+               {
+                   this.settings = PluginSettingsManager.GetSetting<ArticleSettings>("ArticleSettings", new ArticleSettings());
+                   this.Plugin.OnPluginStatusChanged(PluginStatus.Normal, null);
+                   this.articles = ArticleConnector.GetArticlesFromAllUrls().Result
+                       .OrderByDescending(a => a.DateModified)
+                       .ToList();
+               }
+               catch (Exception ex)
+               {
+                   PluginLog.Error("Error reloading articles: " + ex.Message);
+                   this.articles = new List<Article>();
+               }
+           }
+        
+         public override IEnumerable<String> GetButtonPressActionNames(DeviceType _)
+         {
+
+             List<String> result = new List<String>();
+
+             result.Add(PluginDynamicFolder.NavigateUpActionName);
+             
+             foreach (var article in this.articles)
+             {
+                 result.Add(this.CreateCommandName(article.Title));
+             }
+             return result;
+         }
+        public override void RunCommand(String actionParameter)
+        {
+            String url = this.settings.DefaultUrl;
+            foreach (var article in articles)
+            {
+                if(actionParameter == article.Title)
+                {
+                    url = article.Url;
+                    break;
+                }
+            }
+            
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            this.Close();
+        }
+    }
+}
